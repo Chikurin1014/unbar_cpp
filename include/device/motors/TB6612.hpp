dcc6cfc6f9423b28c03f94313abd9e3be15c064f
Chikurin1014 <chikurin1014@gmail.com>
2024-12-21 02:55:26 +0900

    :adhesive_bandage: fix TB6612.hpp

--- include/device/motors/TB6612.hpp
+++ include/device/motors/TB6612.hpp
@@ -7,9 +7,13 @@
 
 namespace device::motors {
 
-template<uint8_t DIR1_PIN, uint8_t DIR2_PIN, uint8_t PWM_PIN>
+template<uint8_t DIR1, uint8_t DIR2, uint8_t PWM>
 class TB6612 {
 public:
+    inline static constexpr uint8_t DIR1_PIN = DIR1;
+    inline static constexpr uint8_t DIR2_PIN = DIR2;
+    inline static constexpr uint8_t PWM_PIN = PWM;
+
     TB6612() {
         pinMode(DIR1_PIN, OUTPUT);
         pinMode(DIR2_PIN, OUTPUT);
@@ -17,7 +21,7 @@ public:
         ledcAttach(PWM_PIN, 12800, 16);
     }
     ~TB6612() {
-        ledcDetachPin(PWM_PIN);
+        ledcDetach(PWM_PIN);
     }
 
     inline auto run(const order::motors::TB6612 &order) {
