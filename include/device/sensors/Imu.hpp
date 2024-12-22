8f8df02c1d1c9b6673790c8942c9110a5c5e1662
Chikurin1014 <chikurin1014@gmail.com>
2024-12-22 23:45:32 +0900

    :adhesive_bandage: Fix Imu.hpp

--- include/device/sensors/Imu.hpp
+++ include/device/sensors/Imu.hpp
@@ -2,17 +2,23 @@
 #define DEVICE_SENSORS_IMU_HPP
 
 #include "Adafruit_BNO055.h"
+#include "Wire.h"
 
 namespace device::sensors {
 
 template<int32_t ID, uint8_t ADDR>
 class Imu {
 private:
+    inline static constexpr uint8_t I2C_SDA = 21;
+    inline static constexpr uint8_t I2C_SCL = 22;
+
     Adafruit_BNO055 imu;
+    TwoWire wire;
 
 public:
     Imu()
-      : imu{ID, ADDR} {
+      : wire{I2C_SDA, I2C_SCL} {
+        this->imu = Adafruit_BNO055{ID, ADDR, &this->wire};
         this->imu.begin();
     }
     ~Imu() = default;
