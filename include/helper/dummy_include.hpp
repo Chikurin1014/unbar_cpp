81af9e6b12501792840550719fa020f65b0af116
Chikurin1014 <chikurin1014@gmail.com>
2024-12-22 13:57:44 +0900

    :adhesive_bandage: fix platformio.ini

---
+++ include/helper/dummy_include.hpp
@@ -0,0 +1,14 @@
+#ifndef HELPER_DUMMY_INCLUDE_HPP
+#define HELPER_DUMMY_INCLUDE_HPP
+
+/**
+ * @brief Dummy inclusion of dependencies to avoid compilation errors.
+ * @details includeしないとコンパイルエラーを起こすヘッダファイルがあるため、それらのヘッダファイルをincludeしておく。
+ */
+
+#include <SPI.h>
+
+#include "Adafruit_GFX.h"
+#include "Adafruit_Sensor.h"
+
+#endif // HELPER_DUMMY_INCLUDE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include "helper/dummy_include.hpp"
+
 void setup() {}
 
 void loop() {}
