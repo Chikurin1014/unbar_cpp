7fbbedd7a21b8160341a2c22ceabf1085df2ffbc
Chikurin1014 <chikurin1014@gmail.com>
2025-01-24 10:19:12 +0900

    :fire: remove main

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,8 @@
 
 #include "Arduino.h"
 
-#include "device/Unbar.hpp"
 #include "helper/dummy_include.hpp"
 
-void setup() {
-    auto unbar = device::Unbar();
-    const auto period = 3000;
-    const auto omega = 2 * std::numbers::pi_v<double> / period;
-    while (true) {
-        auto t = millis();
-        auto theta = omega * (t % period);
-        auto m_order = order::Motors{
-          .left = order::Motors::MotorDriver::normalize(std::cos(theta)),
-          .right = order::Motors::MotorDriver::normalize(std::sin(theta)),
-        };
-        unbar.motors.run(m_order);
-        delay(1);
-    }
-}
+void setup() {}
 
 void loop() {}
